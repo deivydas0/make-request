ae7c5c84f16a11e3db9e1c7d47d67907241b7812
Deivydas Sarcevičius <deivydas.s@commercecore.com>
2024-07-10 14:29:04 +0300

    Catch all requests

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('{any}', function () {
+    return response()->json([
+        'headers' => collect(request()->headers),
+        'body'    => request()->all(),
+    ]);
+})->where('any', '.*');
