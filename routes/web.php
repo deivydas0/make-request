892030b3fb5cc25bcfd4b93de23d6089789e436b
Deivydas Sarcevičius <deivydas.s@commercecore.com>
2024-07-11 10:16:44 +0300

    Allows to request particular response code

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::any('{any}', function () {
+    $desiredResponseCode = intval(request()->input('response_code', 200));
+    $message             = sprintf(
+        '%s: %s',
+        $desiredResponseCode,
+        data_get(Response::$statusTexts, $desiredResponseCode)
+    );
+
     return response()->json([
+        'data'    => request()->all(),
         'headers' => collect(request()->headers),
-        'body'    => request()->all(),
-    ]);
+        'message' => $message,
+        'method'  => request()->method(),
+        'status'  => $desiredResponseCode,
+        'url'     => request()->fullUrl(),
+    ], $desiredResponseCode);
 })->where('any', '.*');
