d1e6fffc2741463e9d8922cfd706dc200fb650b0
Deivydas Sarcevičius <deivydas.s@commercecore.com>
2024-07-12 14:24:45 +0300

    Uses injected $request instead of global request() helper

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,27 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::any('{any}', function () {
-    $desiredResponseCode = intval(request()->input('response_code', 200));
+Route::any('{any}', function (Request $request) {
+    $desiredResponseCode = intval($request->input('response_code', 200));
     $message             = sprintf(
         '%s: %s',
         $desiredResponseCode,
         data_get(Response::$statusTexts, $desiredResponseCode)
     );
 
-    return response()->json([
-        'data'    => request()->all(),
-        'headers' => collect(request()->headers),
+    $responseData = [
+        'data'    => $request->all(),
+        'headers' => collect($request->headers),
         'message' => $message,
-        'method'  => request()->method(),
+        'method'  => $request->method(),
         'status'  => $desiredResponseCode,
-        'url'     => request()->fullUrl(),
-    ], $desiredResponseCode);
+        'url'     => $request->fullUrl(),
+    ];
+
+    // info($responseData);
+
+    return response()->json($responseData, $desiredResponseCode);
 })->where('any', '.*');
